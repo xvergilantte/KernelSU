c5f6ac4b8e44e64fb1f4136824002602d13c79e0
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-06-24 12:30:14 +0800

    kernel: apk_sign: fix return check for ksu_sha256
    upstream used IS_ERR to check for negative return and that is int,
    so correct it.
    
    This is one headache for old compilers.
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/manager/apk_sign.c
+++ kernel/manager/apk_sign.c
@@ -91,7 +91,7 @@ static bool check_block(struct file *fp, u32 *size4, loff_t *pos, u32 *offset,
 			return false;
 		}
 		unsigned char digest[SHA256_DIGEST_SIZE];
-		if (IS_ERR(ksu_sha256(cert, *size4, digest))) {
+		if (ksu_sha256(cert, *size4, digest) < 0 ) {
 			pr_info("sha256 error\n");
 			return false;
 		}
